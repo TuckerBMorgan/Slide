33ad95494073b48c8c3b9ddf07a2f0fbf28b000b
Tucker <Tucker.bull.morgan@gmail.com>
2015-10-10 12:23:35 -0400

    First Commit

---
+++ Assets/SlideCharacter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideCharacter : MonoBehaviour {
+    
+    private float health;
+    private float armour;
+    private int actions;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
